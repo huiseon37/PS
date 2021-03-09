cab2c81068d625e365527404e5faa54033f81ff2
huiseon37 <huiseon37@gmail.com>
2021-03-09 20:35:24 +0900

    BOJ

--- 1000-10000/2751.cpp
+++ 1000-10000/2751.cpp
@@ -1,28 +1,75 @@
+//2751 - 머지소트 이용 
 #include <iostream>
-#include <algorithm>
+#include <cstdio>
 
 using namespace std;
 
 int num[1000001];
+int sortnum[1000001];
+
+void merge(int left, int mid, int right){
+	int i, j, k, l;
+
+	
+	i = left;
+	j = mid+1;
+	k = left;
+	
+	while(i<=mid && j<=right){
+		if(sortnum[i]<=sortnum[j]){
+			num[k] = sortnum[i++];
+		}
+		else{
+			num[k] = sortnum[j++];
+		}
+		k++;
+	}
+	
+	if(i>mid){
+		for(l=j; l<=right; l++){
+			num[k++] = sortnum[l];
+		}
+	}
+	else{
+		for(l=i; l<=mid;l++){
+			num[k++] = sortnum[l];
+		}
+	}
+	
+	for(l=left; l<=right; l++){
+		sortnum[l] = num[l];
+	}
+}
+
+void mergeSort(int left, int right){
+	
+	if(left<right) {
+	
+		int mid = (left+right)/2;
+		mergeSort(left,mid);
+		mergeSort(mid+1,right);
+		merge(left, mid, right);
+	}
+}
 
 int main(){
 	
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	cout.tie(NULL);
 	
 	int n;
 	
-	cin>>n;
+	scanf("%d",&n);
 	
 	for(int i=0;i<n;i++){
-		cin>>num[i];
+		scanf("%d",&sortnum[i]);
 	}
 	
-	sort(num,num+n);
+	mergeSort(0,n-1);
 	
 	for(int i=0;i<n;i++){
-		cout<<num[i]<<endl;
+		printf("%d\n",sortnum[i]);
 	}
+	
 	return 0;
 }
