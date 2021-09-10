c2e91a9999ac34a74b45a3a818533bf1fee1dbc0
huiseon37 <huiseon37@gmail.com>
2021-09-10 21:06:27 +0900

    BOJ

---
+++ 1000-10000/2741.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(void){
+	
+	
+	int n;
+	
+	cin>>n;
+	
+	for(int i=0;i<n;i++){
+		printf("%d\n",i+1);
+	}
+	
+	return 0;
+}
