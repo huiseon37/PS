4591ec74a202fa79ddbe75b765475b81e1edcc33
huiseon37 <huiseon37@gmail.com>
2021-09-20 19:32:40 +0900

    BOJ 합

---
+++ 1000-10000/8393.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	
+	int n;
+	
+	cin>>n;
+	
+	int sum = 0;
+	
+	for(int i=0;i<n;i++){
+		sum += i+1;	
+	}
+	
+	cout<<sum<<endl;
+		
+	return 0;
+}
