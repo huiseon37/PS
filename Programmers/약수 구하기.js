a32d146b07bc911b8f5b01ef946e940e499f4e11
sunny <huiseon37@gmail.com>
2023-06-02 21:38:15 +0900

    Programmers 약수 구하기

---
+++ Programmers/약수 구하기.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = new Set();
+    for(let i=1; i<=n; i++){
+        if(n%i === 0){
+            answer.add(i);
+            answer.add(n/i)
+        }
+    }
+    return [...answer].sort((a,b)=>a-b);
+}
