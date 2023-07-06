0f0dd5cb78374a72c9277d4245ee0e03da77d751
sunny <huiseon37@gmail.com>
2023-07-06 20:23:41 +0900

    Programmers 겹치는 선분의 길이

--- Programmers/겹치는 선분의 길이.js
+++ Programmers/겹치는 선분의 길이.js
@@ -1,30 +1,18 @@
 function solution(lines) {
-    let answer = [];
-    while(lines.length){
-        const [x1,y1] = lines.shift();
-        lines.forEach(([x2,y2])=>{
-            const tmp = [Math.max(x1,x2), Math.min(y1,y2)];
-            if(tmp[0] < tmp[1]){
-                // answer.forEach((el)=>{
-                //     if((el[0]<=tmp[0] && el[1]>=tmp[0]) || (el[0]<=tmp[1] && el[1]>=tmp[1])){
-                //         answer = [...answer, ]
-                //     }
-                // })
-                answer = [...answer, tmp];
-            }
-        })
-    }
+    let overlapLines = {};
     
-    if(!answer.length){
-        return 0;
-    }
-    
-    while(true){
-        if(answer.length === 1){
-            return answer[0][1] - answer[0][0];
+    lines.forEach(([start,end])=>{
+        for(let i= start; i < end; i++ ){
+            const key = ''+i+(i+1);
+            overlapLines[key] = overlapLines[key] ? overlapLines[key] + 1 : 1;
         }
-        const test1 = answer.shift();
-        const test2 = answer.shift();
-        answer = [...answer, [Math.min(test1[0], test2[0]), Math.max(test1[1], test2[1])]]
-    }
+    })
+    
+    let answer = 0;
+    
+    Object.values(overlapLines).forEach((val)=>{
+        answer = val > 1 ? answer + 1 : answer; 
+    })
+    
+    return answer;
 }
